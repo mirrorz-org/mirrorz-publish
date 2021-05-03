97c18e23100a3c08fe504c378b4b4ed5febdc435
ZenithalHourlyRate <ZenithalHourlyRate@users.noreply.github.com>
2021-05-03 12:08:14 +0000

    Github Action auto build for https://github.com/tuna/mirrorz/commit/3c9bcf7

--- oh-my-mirrorz.py
+++ oh-my-mirrorz.py
@@ -8,12 +8,12 @@ import sys
 import os
 import argparse
 
-VERSION = '20210409'
+VERSION = '20210503'
 CURL_VERSION = ''
 
 big = {
     'centos': '/8/isos/x86_64/CentOS-8.3.2011-x86_64-dvd1.iso',
-    'archlinux': '/iso/latest/archlinux-2021.03.01-x86_64.iso',
+    'archlinux': '/iso/latest/archlinux-2021.05.01-x86_64.iso',
     'ubuntu': '/indices/md5sums.gz',
     'debian': '/ls-lR.gz',
 }
